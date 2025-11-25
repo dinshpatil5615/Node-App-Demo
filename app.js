160748f0d21541d24f41cbb08e2f09ea42ab8bc7
dinshpatil5615 <126959178+dinshpatil5615@users.noreply.github.com>
2025-11-25 20:22:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("CI/CD working fine! Hello from jenkins + Docker");
+  res.send("CI/CD working fine! Hello from jenkins + Docker and pushing image on dockerhub");
 });
 
 app.listen(3000, () => {
